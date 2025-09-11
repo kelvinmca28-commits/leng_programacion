8be272068aa9bd4c1fab133880f520552fac8140
kelvinmca28-commits <kelvin.mca28@gmail.com>
2025-09-10 21:19:45 -0500

    Add files via upload

---
+++ ejercicio1_10-.09.py
@@ -0,0 +1,26 @@
+def cuadrado_y_cubo(numero):
+    return numero ** 2, numero ** 3
+cuad, cubo = cuadrado_y_cubo(4)
+print(cuad)
+print(cubo)
+
+print()
+def exponentes(numero):
+    return numero ** 3, numero ** 5, numero ** 9
+x=int(input("Indique el numero a conciderar: "))
+cubo,quinta,novena = exponentes(x)
+print("Al cubo:", cubo)
+print("A la quinta:", quinta)
+print("A la novena:", novena)
+
+print()
+def exponentes(numero,a,b,c):
+    return numero ** a, numero ** b, numero ** c
+x=int(input("Indique el numero a conciderar: "))
+a=int(input("primer exponente: "))
+b=int(input("segundo exponente: "))
+c=int(input("tercer exponente: "))
+prim,seg,ter = exponentes(x,a,b,c)
+print("elevado a:", a, "sale:", prim)
+print("elevado a:", b, "sale:", seg)
+print("elevado a:", c, "sale:", ter)
---
+++ ejercicio2_10_09.py
@@ -0,0 +1,49 @@
+def tabla_del(numero):
+    resultados = []
+    for i in range(11):
+        resultados.append(numero * i)
+    return resultados
+res = tabla_del(3)
+print(res)
+
+print()
+def jose():
+    num=int(input("que producto compraste 1)leche 2)pan 3)alcohol 4)otros; "))
+    total=int(input("cuanto costo S/.? : "))
+    if num==1:
+        im=10
+    elif num==2:
+        im=20
+    elif num==3:
+        im=30
+    else:
+        im=50
+    impu=(total*im/100)
+    print('impuesto de ese producto es S/.: ',impu)
+# print(impu)
+    return impu
+jose()
+print("Programa terminado")
+
+print()
+def pago_mensual():
+    grado=int(input("En que grado esta el niño: 1)primero 2)segundo 3)tercero 4)cuarto 5)quinto 6)sexto; "))
+    if grado==1:
+        pago=400
+    elif grado==2:
+        pago=450
+    elif grado==3:
+        pago=500
+    elif grado==4:
+        pago=450
+    elif grado==5:
+        pago=500
+    else:
+        pago=550
+    impu=(pago*5/100)
+    mensualidad=(pago+impu)
+    print("el impuesto seria:", impu)
+    print('la mensulidad es de: S/.',mensualidad)
+    return mensualidad
+pago_mensual()
+print("Programa terminado")
